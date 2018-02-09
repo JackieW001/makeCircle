b53e25c4c4efc3fb0f503f9595bb73977b148ffc
JackieW001 <jwoo@stuy.edu>
2018-02-09 10:51:46 -0500

    bouncy boi

--- makecircle.js
+++ makecircle.js
@@ -1,13 +1,15 @@
 var canvas = document.getElementById('canvas');
+var shrinkButton = document.getElementById('shrink');
+var bounceButton = document.getElementById('bounce');
 var stopButton = document.getElementById('stop');
 var clear = document.getElementById('clear');
-var toggle = document.getElementById('toggle');
+
 
 var ctx = canvas.getContext('2d');
 var pen = 0;
 var requestID;
 
-var animate = function(){
+var shrink = function(){
 
     window.cancelAnimationFrame(requestID);
     
@@ -43,7 +45,57 @@ var animate = function(){
     }
     draw();
 }
-canvas.addEventListener('click', animate);
+shrinkButton.addEventListener('click', shrink);
+
+//var posX = 250;
+//var posY = 250;
+var bounce = function(){
+    window.cancelAnimationFrame(requestID);
+    var posX = 250;
+    var posY = 250;
+    var radius = 20;
+    var moveX = Math.floor(Math.random()*10)+1;
+    var moveY = Math.floor(Math.random()*10)+1;
+    
+    var draw = function(){
+	clearCanvas();
+	
+	ctx.strokeStyle="pink";
+	ctx.fillStyle="lightblue";
+	ctx.arc(posX, posY, radius, 0, 2*Math.PI);
+	ctx.fill();
+	ctx.stroke();
+	
+	posX += moveX;
+	posY += moveY;
+	
+	//bottom
+	if (posY >= canvas.height-radius){
+	    moveY = -moveY;
+	}
+
+	//right
+	if (posX >= canvas.width-radius){
+	    moveX = -moveX;
+	}
+
+	//left
+	if (posX <= 0+radius){
+	    moveX = -moveX;
+	}
+
+	//top
+	if (posY <= 0+radius){
+	    moveY = -moveY;
+	}
+	
+	requestID = window.requestAnimationFrame( draw );
+	console.log(requestID);
+    }
+    draw();
+    
+}
+bounceButton.addEventListener('click', bounce);
 
 var stopit = function(){
     window.cancelAnimationFrame(requestID);
@@ -57,13 +109,3 @@ var clearCanvas = function(e){
     ctx.closePath();
 }
 clear.addEventListener('click', clearCanvas);
-
-var togglePen = function(e){
-    if (pen == 0){
-	pen = 1;
-    }
-    else{
-	pen = 0;
-    }
-}
-toggle.addEventListener('click', togglePen);
